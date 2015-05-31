b70170b42ad25b8770c435fd5717992f57328447
david grupp <david@grupp.me>
2015-05-31 12:44:26 -0500

    updated reprepo to return just content

--- lib/repcountdown/RepRepo.ex
+++ lib/repcountdown/RepRepo.ex
@@ -2,17 +2,25 @@ defmodule Repcountdown.RepRepo do
 
   def get_user_rep(id) when is_integer(id) do
     Task.async(fn->
-      %{body: resp, status_code: 200} =
-        HTTPoison.get! "http://api.stackexchange.com/2.2//2.2/users/{#id}?order=desc&sort=reputation&site=stackoverflow"
-      resp
+      resp = HTTPoison.get! "http://api.stackexchange.com/2.2/users/#{id}?order=desc&sort=reputation&site=stackoverflow"
+      case(resp) do
+        %{body: content, status_code: 200, headers: %{ "Content-Encoding" => "gzip" }} -> 
+          :zlib.gunzip(content)
+        %{body: content, status_code: 200, headers: %{ "Content-Encoding" => _ }} -> 
+          content
+      end
     end)  
   end
 
   def get_user_rep(name) when is_binary(name) do
     Task.async(fn->
-      %{body: resp, status_code: 200} = 
-        HTTPoison.get! "http://api.stackexchange.com/2.2/users?order=desc&sort=reputation&inname={#name}&site=stackoverflow"
-      resp
+      resp = HTTPoison.get! "http://api.stackexchange.com/2.2/users?order=desc&sort=reputation&inname=#{name}&site=stackoverflow" 
+      case(resp) do
+        %{body: content, status_code: 200, headers: %{ "Content-Encoding" => "gzip" }} -> 
+          :zlib.gunzip(content)
+        %{body: content, status_code: 200, headers: %{ "Content-Encoding" => _ }} -> 
+          content
+      end
     end)  
   end
   
