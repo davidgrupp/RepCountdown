ed561a0c83c335a0d09713d961fd19fe105802a5
david grupp <david@grupp.me>
2015-06-01 19:26:31 -0500

    began cache

--- lib/repcountdown/RepCache.ex
+++ lib/repcountdown/RepCache.ex
@@ -1,11 +1,21 @@
 defmodule Repcountdown.RepCache do
-
-  def handle_call(:pop, _from, [h|t]) do
-    {:reply, h, t}
+  use GenServer
+  
+  def get_user_rep(name) when is_binary do
+    GenServer.call(__MODULE__, {:get, name})
+  end
+  
+  def put_user_rep(name, rep) do
+    
+    GenServer.cast(__MODULE__,{:put, name, rep})
+  end
+  
+  def handle_call({:get, name}, _from, hd) do
+    {:reply, hd.get(name), hd}
   end
 
-  def handle_cast({:push, item}, state) do
-    {:noreply, [item|state]}
+  def handle_cast({:put, name, rep, repHigh, repLow}, hd) do
+    {:noreply, hd.put(name,{rep,repHigh, repLow)}
   end
 
 end
